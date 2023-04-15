aae84c9c7cc64ef68ea384558b631d8056840ec9
Rizart Dokollari <r.dokollari@gmail.com>
2023-04-16 00:24:10 +0100

    Set the game id

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,3 +1,21 @@
-it("ensures tests are working", () => {
-  expect(true).toBeTruthy();
+import { createMock } from "ts-auto-mock";
+import { IExtensionContext } from "vortex-api/lib/types/IExtensionContext";
+import main from "../index";
+
+describe("Game registration", () => {
+  const { vortexContext } = makeFactory();
+  main(vortexContext);
+
+  it("sets the id", async () => {
+    expect(vortexContext.registerGame).toHaveBeenCalledWith(
+      expect.objectContaining({ id: "talesofarise" })
+    );
+  });
 });
+
+function makeFactory() {
+  const vortexContext = createMock<IExtensionContext>({
+    registerGame: jest.fn(),
+  });
+  return { vortexContext };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { types } from "vortex-api";
 
-const main = ({ registerAction, api }: types.IExtensionContext) => {
-  registerAction("global-icons", 100, "menu", {}, "Sample", () => {
-    api.showDialog &&
-      api.showDialog(
+const main = (context: types.IExtensionContext) => {
+  context.registerGame({
+    id: "talesofarise",
+    queryModPath: function (gamePath: string): string {
+      throw new Error("Function not implemented.");
+    },
+    mergeMods: false,
+    name: "",
+    executable: function (discoveredPath?: string | undefined): string {
+      throw new Error("Function not implemented.");
+    },
+    requiredFiles: [],
+  });
+
+  context.registerAction("global-icons", 100, "menu", {}, "Sample", () => {
+    context.api.showDialog &&
+      context.api.showDialog(
         "info",
         "Success!",
         {
