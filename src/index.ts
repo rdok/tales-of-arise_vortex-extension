4cd2079ba594174948438bbb1c3ca587a5180c09
Rizart Dokollari <r.dokollari@gmail.com>
2023-04-16 02:34:46 +0100

    Remove unit test due to vortex-api module not found by test & low value.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { types } from "vortex-api";
+import { GameStoreHelper as gameStoreHelper } from "vortex-api/lib/util/api";
 import { GameRegistration } from "./GameRegistration";
 
-const gameRegistration = new GameRegistration();
-
 const main = (context: types.IExtensionContext) => {
+  const gameRegistration = new GameRegistration({ gameStoreHelper });
   gameRegistration.run(context);
   context.registerAction("global-icons", 100, "menu", {}, "Sample", () => {
     context.api.showDialog &&
