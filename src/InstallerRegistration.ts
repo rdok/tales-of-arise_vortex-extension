2203b1338edc80d7a23cb63e4e3abf67edcc9c14
Rizart Dokollari <r.dokollari@gmail.com>
2023-04-16 05:57:34 +0100

    Test support content & install content

--- src/InstallerRegistration.ts
+++ src/InstallerRegistration.ts
@@ -1,27 +1,60 @@
+import path from "path";
 import { util } from "vortex-api";
-import { ID } from "./main";
+import { TALESOFARISE_ID } from "./main";
 
 type Props = { gameStoreHelper: typeof util.GameStoreHelper };
 
 export class InstallerRegistration {
   private gameStoreHelper: any;
+  private modFileExt = ".pak";
 
   constructor(props: Props) {
     this.gameStoreHelper = props.gameStoreHelper;
   }
 
   create() {
-    return [
-      `${ID}-PAK`,
-      25,
-      true,
-      [
-        {
-          type: "copy",
-          source: "source_path",
-          destination: "destination",
-        },
-      ],
-    ];
+    const installerName = `${TALESOFARISE_ID}-PAK`;
+    const priority = 25;
+    const testSupportedContent = this.testSupportedContent;
+    const installContent = this.installContent;
+
+    return { installerName, priority, testSupportedContent, installContent };
   }
+
+  testSupportedContent = (files: string[]) => {
+    let supported =
+      files.find(
+        (file) => path.extname(file).toLowerCase() === this.modFileExt
+      ) !== undefined;
+
+    return Promise.resolve({
+      supported,
+      requiredFiles: [],
+    });
+  };
+
+  installContent = (files: string[]) => {
+    const modFile = files.find(
+      (file) => path.extname(file).toLowerCase() === this.modFileExt
+    );
+
+    if (!modFile) return Promise.resolve({ instructions: [] });
+
+    const idx = modFile.indexOf(path.basename(modFile));
+    const rootPath = path.dirname(modFile);
+
+    const filtered = files.filter(
+      (file) => file.indexOf(rootPath) !== -1 && !file.endsWith(path.sep)
+    );
+
+    const instructions = filtered.map((file) => {
+      return {
+        type: "copy",
+        source: file,
+        destination: path.join(file.substr(idx)),
+      };
+    });
+
+    return Promise.resolve({ instructions });
+  };
 }
--- src/__tests__/InstallerRegistration.spec.ts
+++ src/__tests__/InstallerRegistration.spec.ts
@@ -3,28 +3,45 @@ import { makeVortexApi } from "../../jest/factories";
 
 describe("InstallerRegistration", () => {
   const { installerRegistration } = makeFactory();
-  const installer = installerRegistration.create();
+  const { installerName, priority, testSupportedContent, installContent } =
+    installerRegistration.create();
 
   it("sets the installer name", async () => {
-    expect(installer[0]).toEqual("talesofarise-PAK");
+    expect(installerName).toEqual("talesofarise-PAK");
   });
 
   it("sets the priority", async () => {
-    expect(installer[1]).toEqual(25);
+    expect(priority).toEqual(25);
   });
 
-  it("sets a valid content qualifier function", async () => {
-    expect(installer[2]).toBeTruthy();
+  describe("testSupportedContent", () => {
+    it("sets a valid content qualifier function", async () => {
+      expect(await testSupportedContent(["lorem.pAk"])).toEqual({
+        requiredFiles: [],
+        supported: true,
+      });
+    });
+
+    it("sets an invalid content qualifier function", async () => {
+      const { installerRegistration } = makeInvalidContentQualifierFactory();
+      const { testSupportedContent } = installerRegistration.create();
+      expect(await testSupportedContent([])).toEqual({
+        requiredFiles: [],
+        supported: false,
+      });
+    });
   });
 
-  it("installs the files", async () => {
-    expect(installer[3]).toEqual([
-      {
-        type: "copy",
-        source: "source_path",
-        destination: "destination",
-      },
-    ]);
+  it("installs valid files files", async () => {
+    expect(await installContent(["lorem.pak"])).toEqual({
+      instructions: [
+        { destination: "lorem.pak", source: "lorem.pak", type: "copy" },
+      ],
+    });
+  });
+
+  it("installs no files having non valid", async () => {
+    expect(await installContent(["invalid"])).toEqual({ instructions: [] });
   });
 });
 
@@ -32,4 +49,9 @@ function makeFactory() {
   const { gameStoreHelper, iGameStoreEntry } = makeVortexApi();
   const installerRegistration = new InstallerRegistration({ gameStoreHelper });
   return { installerRegistration, gameStoreHelper, iGameStoreEntry };
+}
+function makeInvalidContentQualifierFactory() {
+  const factory = makeFactory();
+
+  return { ...factory };
 }
